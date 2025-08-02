60464c8068b2a6b71ba20e6501a3076595333258
hikaripipi <hikarishinagawa@cryptogames.co.jp>
2025-08-02 19:17:36 +0900

    Implement character sending and receiving functionality in minitalk client and server

--- milestone3/minitalk/src/client.c
+++ milestone3/minitalk/src/client.c
@@ -6,11 +6,64 @@
 /*   By: hikarimac <hikarimac@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/29 18:56:57 by hikarimac         #+#    #+#             */
-/*   Updated: 2025/07/30 21:35:28 by hikarimac        ###   ########.fr       */
+/*   Updated: 2025/08/02 19:15:17 by hikarimac        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minitalk.h"
+
+char	*char_to_binary(char c)
+{
+	char	*binary;
+	int		ascii_value;
+	int		i;
+
+	ascii_value = (int)c;
+	binary = ft_calloc(9, sizeof(char));
+	if (!binary)
+		return (NULL);
+	i = 7;
+	while (i >= 0)
+	{
+		binary[i] = (ascii_value % 2) + '0';
+		ascii_value = ascii_value / 2;
+		i--;
+	}
+	binary[8] = '\0';
+	return (binary);
+}
+
+void	send_char(char c, int pid)
+{
+	char	*bmassage;
+	int		count;
+
+	bmassage = char_to_binary(c);
+	count = 0;
+	while (count < 8)
+	{
+		if (bmassage[count] == '0')
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		usleep(1000);
+		count++;
+	}
+	free(bmassage);
+}
+
+void	send_massage(int pid, char *massage)
+{
+	int	i;
+
+	i = 0;
+	while (massage[i] != '\0')
+	{
+		send_char(massage[i], pid);
+		i++;
+	}
+	send_char('\0', pid); // null終端文字も同じ関数で！
+}
 
 int	main(int argc, char **argv)
 {
@@ -19,6 +72,7 @@ int	main(int argc, char **argv)
 		ft_printf("Error: %s need correct input\n", argv[0]);
 		return (1);
 	}
-	kill(ft_atoi(argv[1]), SIGUSR1);
+	send_massage(ft_atoi(argv[1]), argv[2]);
+	// kill(ft_atoi(argv[1]), SIGUSR1);
 	return (0);
 }
--- milestone3/minitalk/src/server.c
+++ milestone3/minitalk/src/server.c
@@ -6,25 +6,63 @@
 /*   By: hikarimac <hikarimac@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/29 18:56:54 by hikarimac         #+#    #+#             */
-/*   Updated: 2025/07/30 21:34:56 by hikarimac        ###   ########.fr       */
+/*   Updated: 2025/08/02 02:15:38 by hikarimac        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minitalk.h"
 
-void	handler(int num)
+static char	current_char = 0;
+static int	bit_count = 0;
+static char	message[1000];
+static int	char_count = 0;
+
+void	handler1(int sig)
 {
-	(void)num;
-	ft_printf("got signal\n");
+	(void)sig;
+	current_char = current_char << 1;
+	bit_count++;
+	if (bit_count == 8)
+	{
+		message[char_count] = current_char;
+		char_count++;
+		if (current_char == '\0')
+		{
+			ft_printf("Received: %s\n", message);
+			char_count = 0;
+		}
+		current_char = 0;
+		bit_count = 0;
+	}
+}
+
+void	handler2(int sig)
+{
+	(void)sig;
+	current_char = (current_char << 1) | 1;
+	bit_count++;
+	if (bit_count == 8)
+	{
+		message[char_count] = current_char;
+		char_count++;
+		if (current_char == '\0')
+		{
+			ft_printf("Received: %s\n", message);
+			char_count = 0;
+		}
+		current_char = 0;
+		bit_count = 0;
+	}
 }
 
 int	main(void)
 {
+	signal(SIGUSR1, handler1);
+	signal(SIGUSR2, handler2);
+	ft_printf("Server PID: %d\n", getpid());
 	while (1)
 	{
-		signal(SIGUSR1, handler);
-		ft_printf("Server PID: %d\n", getpid());
-		sleep(1);
+		pause();
 	}
 	return (0);
 }
